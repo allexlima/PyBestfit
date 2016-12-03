bcb7a576d1da3cee2d8433db4d1591946bd443d5
Allex Lima <allexlima97@gmail.com>
2016-12-03 16:52:32 -0400

    Restructuring of classes and the project's main package

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 # By Allex Lima <allexlima@unn.edu.br> | www.allexlima.com
 
-import m_manager as myos
+import nano_os as myos
 import json
 
 if __name__ == "__main__":
+    memory = myos.Memory()
     process_m = myos.ProcessManager()
     p1 = process_m.create("teste_p1.cpp", "juca")
     p2 = process_m.create("teste_p2.py", "allex")
@@ -13,5 +14,5 @@ if __name__ == "__main__":
     # print "PID dos processos criados: {0}, {1} e {2}. \nApagando processo {1}...\n".format(p1, p2, p3)
     process_m.kill(p2)
     # print json.dumps(process_m.show(p2), indent=4, sort_keys=True)
-    memory = myos.Memory()
-    print json.dumps(memory.show(), indent=4, sort_keys=True)
+    # print json.dumps(memory.show(), indent=4, sort_keys=True)
+
--- m_manager/allocators.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# By Allex Lima <allexlima@unn.edu.br> | www.allexlima.com
-
-if __name__ == "__main__":
-    pass
